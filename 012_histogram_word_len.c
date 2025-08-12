07d1725241382225efeb2d37a06c8e7e83fab51f
97-vinash <97.vinash@gmail.com>
2025-08-12 07:47:59 +0000

    012 - till max char
    Added to only limit the count to the maximum number of char,
    i.e. any char lets say there are 1,2,3,4 cahr words.
    if there is no 3 char words but the max char word is 4,
    then 3 will be shown as 0/nothing, and if 4 is max char word in the input/file,
    so after that if all are 0, means no words with 5,6,7..chars,
    then last number will only be till 4 char words.

--- 012_histogram_word_len.c
+++ 012_histogram_word_len.c
@@ -1,9 +1,10 @@
+// Exercise 1-13
 #include <stdio.h>
 #include <stdbool.h>
 
 int main(){
     
-    int str, count = 0;
+    int str, count = 0, max_count = 0;
     int length[21] = {0};
     bool flag = true;
     while((str = getchar()) != EOF){
@@ -13,16 +14,32 @@ int main(){
         }
         else if(flag == false) continue;
         else {
+            if(max_count < count) max_count = count;
             ++length[count];
-            // printf("%d\n", count);
             count = 0;
             flag = false;
         }
     }
 
-    for(int i = 1;i <= 20;i++){
-        printf("%2d character words  -->  %d\n", i, length[i]);
+    for(int i = 1;i <= max_count;i++){
+        printf("%2d: ", i);
+        for(int j = 1;j <= length[i];j++){
+            printf("*");
+        }
+        printf("\n");
     }
 
     return 0;
 }
+
+/*
+One thing i added was to only limit the count to the maximum number of char,
+i.e. any char lets say there are 1,2,3,4 cahr words.
+if there is no 3 char words but the max char word is 4,
+then 3 will be shown as 0/nothing, and if 4 is max char word in the input/file,
+so after that if all are 0, means no words with 5,6,7..chars,
+then last shows will only be till 4 char words.
+
+It took some tweaking to make it, and the code maybe a little complicated than it had to,
+but this is the way i figured out by breaking it into smaller parts and adding everything together.
+*/
